5d76623377d8d134789c9e636d6132d4376c1247
Ben Dunlap <ben@adjusterpro.com>
2023-01-16 09:24:47 -0600

    replace tabs with spaces //bdunlap

--- be-quiet-bell.js
+++ be-quiet-bell.js
@@ -1,11 +1,11 @@
 javascript:(
-	function(){
-		var clearTab = function(){
-			var title=document.title.replace(/^\([0-9]+\) */, '');
-			document.title=title;
-		};
-		$('.surface-notifications-component .item-counter').hide();
-		clearTab();
-		setInterval(clearTab, 1000);
-	}
+  function(){
+    var clearTab = function(){
+      var title=document.title.replace(/^\([0-9]+\) */, '');
+      document.title=title;
+    };
+    $('.surface-notifications-component .item-counter').hide();
+    clearTab();
+    setInterval(clearTab, 1000);
+  }
 )()
